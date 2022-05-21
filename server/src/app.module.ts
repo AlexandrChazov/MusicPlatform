2b10d42535a810c0b1c325823abbe86bb17eab64
Александр Чазов <chazovby@gmail.com>
2022-05-21 10:11:52 +0300

    feat: connect to data base

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
-import {TrackModule} from "./track/track.module";
+import { TrackModule } from "./track/track.module";
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [TrackModule]
+  imports: [
+    TrackModule,
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'MusicPlatform',
+      models: [],
+      autoLoadModels: true // чтобы Sequelize автоматически создавал таблицы в БД на основании моделей, которые мы передали выше
+    }),
+  ]
 })
 export class AppModule {}
