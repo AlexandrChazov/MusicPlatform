b76900f52bfb3d9c6c8b3affacac390bae5a4cb4
Александр Чазов <chazovby@gmail.com>
2022-05-21 10:57:11 +0300

    feat: configure environment variables

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TrackModule } from "./track/track.module";
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
     TrackModule,
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'MusicPlatform',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       models: [],
       autoLoadModels: true // чтобы Sequelize автоматически создавал таблицы в БД на основании моделей, которые мы передали выше
     }),
