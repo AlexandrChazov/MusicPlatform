fba253b5d0356cb005ed9d06c472a8d702ffc297
Александр Чазов <chazovby@gmail.com>
2022-05-21 09:09:37 +0300

    feat: add track module

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function start() {
+  try {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule);
+    await app.listen(PORT, () => console.log(`server has started on port ${PORT}`)  );
+  } catch(err) {
+    console.log(err)
+  }
 }
-bootstrap();
+
+start();
